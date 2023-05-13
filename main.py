1d62c4ab327836b700321ec53b21ad0399222ead
Nick5006 <111802560+Nick5006@users.noreply.github.com>
2023-05-13 18:33:48 +0300

    v2

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from pygame import *
+from random import randint
+
+once = False
+win1 = False
+win2 = False
+up = False
+down = False
+right = False
+left = False
 
 
 class GameSprite(sprite.Sprite):
@@ -11,6 +20,7 @@ class GameSprite(sprite.Sprite):
         self.rect.y = y
         self.width = width
         self.height = height
+
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
@@ -33,23 +43,104 @@ class Player2(GameSprite):
             self.rect.y += self.speed
 
 
+class Ball(GameSprite):
+
+    def update(self):
+        global win1, right, left, down, up
+        global win2
+        global once
+
+        if not once:
+
+            er = randint(1, 2)
+            er1 = randint(1, 2)
+            if er == 1:
+                left = True
+            if er == 2:
+                right = True
+            if er1 == 1:
+                up = True
+            if er1 == 2:
+                down = True
+            print(er, er1)
+            once = True
+        elif once:
+            pass
+
+        ##########################################
+        if self.rect.x <= 0:
+            win2 = True
+        if self.rect.x >= 700:
+            win1 = True
+        if self.rect.y <= 0:
+            print(6)
+            up = False
+            down = True
+
+        if self.rect.y >= 700:
+            print('5')
+            up = True
+            down = False
+        col1 = sprite.collide_rect(self, player1)
+        col2 = sprite.collide_rect(self, player2)
+        if col1:
+            if up:
+                up = False
+                down = True
+            if down:
+                up = True
+                down = False
+            if left:
+                left = False
+                right = True
+            if right:
+                left = True
+                right = False
+
+        if col2:
+            if up:
+                up = False
+                down = True
+            if down:
+                up = True
+                down = False
+            if left:
+                left = False
+                right = True
+            if right:
+                left = True
+                right = False
+
+        ##########################################
+        if left:
+            self.rect.x -= self.speed
+        if right:
+            self.rect.x += self.speed
+        if up:
+            self.rect.y -= self.speed
+        if down:
+            self.rect.y += self.speed
+
+
 window = display.set_mode((700, 500))
 display.set_caption('pingpong')
 display.set_icon(image.load('мяч-transformed.png'))
 window.fill((255, 255, 255))
 
 clock = time.Clock()
-player1 = Player1(1, 'блок1-transformed.png', 100, 250, 20, 130)
-player2 = Player2(1, 'блок2-transformed.png', 600, 250, 20, 130)
+player1 = Player1(2, 'блок1-transformed.png', 100, 250, 20, 130)
+player2 = Player2(2, 'блок2-transformed.png', 600, 250, 20, 130)
+ball = Ball(1, 'мяч-transformed.png', 350, 250, 50, 49)
 
 while True:
 
     window.fill((255, 255, 255))
     player1.reset()
     player2.reset()
+    ball.reset()
     player1.update()
     player2.update()
-
+    ball.update()
     for e in event.get():
         if e.type == QUIT:
             exit()
